38d357c310bd77186cea221856cfaa7a5666530c
idroz <idrozdov@beringresearch.com>
2020-08-12 15:32:43 +0100

    move image resize out of the default preproc fx

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='datasets',
-      version='0.2',
+      version='0.3',
       description='Dataset management classes for Tensorflow',
       url='http://github.com/beringresearch/datasets',
       author='Bering Limited',
